effdcf20c30fe88bc67a4893b141c1d23e1d124d
Kevin Chapman <kevchapman@mac.com>
2016-04-06 21:36:10 +0100

    tweaked build task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,9 @@
+var gulp = require('gulp');
+var del = require('del');
+var runSequence = require('run-sequence').use(gulp);
+
 require('gulp-load-tasks')('gulp-tasks');
+
+gulp.task('build', callback => {
+	runSequence('clean-dist', 'build-assets', callback);
+});
