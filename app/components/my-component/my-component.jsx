291e62351d64dc0f32f1cf8e3d879ff3dca4e3d3
jrothpearl <jrothpearl@deloitte.com>
2016-08-16 17:55:37 -0400

    refactor feedback confidence row into its own module

--- app/components/my-component/my-component.jsx
+++ app/components/my-component/my-component.jsx
@@ -9,6 +9,8 @@ import axios from 'axios';
 
 
 import Thermometer from '../thermometer/thermometer.jsx';
+import FeedbackConfidenceRow from '../feedbackConfidenceRow/feedback-confidence.jsx';
+
 
 const popoverHoverFocus = (
   <Popover id="popover-trigger-hover-focus" title="Buy New Device & Services">
@@ -317,44 +319,8 @@ export default class MainDashboard extends React.Component {
 		                <div className="col-md-2 negative">Competitors, Discounts and Offers, Affordability for Price</div>
 		              </div>{/* /Text Analytics Row */}
 		              {/* Feedback Confidence */}
-		              <div className="row row-eq-height">
-		                <div className="col-md-1 row-header">Feedback Confidence</div>
-		                <div className="col-md-1">
-		                  <svg width="39px" height="33px" viewBox="558 0 39 33" version="1.1" >
-		                    <defs />
-		                    <g id="Chart" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(577.500000, 16.500000) rotate(-90.000000) translate(-577.500000, -16.500000) translate(561.000000, -3.000000)">
-		                      <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#D0D0CE" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#F47C6D" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-		                    </g>
-		                  </svg>
-		                </div>
-		                <div className="col-md-1">
-		                  <svg width="39px" height="33px" viewBox="446 0 39 33" version="1.1">
-		                    <defs />
-		                    <g id="Chart-Green" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(465.500000, 16.500000) rotate(-90.000000) translate(-465.500000, -16.500000) translate(449.000000, -3.000000)">
-		                      <rect id="Rectangle-path" fill="#CBD959" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-		                    </g>
-		                  </svg>
-		                </div>
-		                <div className="col-md-1">
-		                  <svg width="39px" height="33px" viewBox="781 0 39 33" version="1.1" >
-		                    <defs />
-		                    <g id="Chart-" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(800.500000, 16.500000) rotate(-90.000000) translate(-800.500000, -16.500000) translate(784.000000, -3.000000)">
-		                      <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-		                      <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-		                    </g>
-		                  </svg>
-		                </div>
-		                <div className="col-md-1" ></div>
-		                <div className="col-md-1" ></div>
-										<div className="col-md-1" ></div>
-										<div className="col-md-1" ></div>
-										<div className="col-md-1" ></div>
-		              </div> {/* /Feedback Confidence Row */}
+                  <FeedbackConfidenceRow />
+		              {/* /Feedback Confidence Row */}
 									<div className="thermometer">
 										<Thermometer />
 									</div>
