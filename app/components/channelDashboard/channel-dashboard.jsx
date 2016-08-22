480e1ebdc7d19ecea84170d121ab53f6ba6f09ee
jrothpearl <jrothpearl@deloitte.com>
2016-08-21 23:16:38 -0400

    three grid item styles finished

--- app/components/channelDashboard/channel-dashboard.jsx
+++ app/components/channelDashboard/channel-dashboard.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router';
 import ReactGridLayout from 'react-grid-layout';
 import {Responsive, WidthProvider} from 'react-grid-layout';
 import { VictoryPie } from 'victory';
+import ChartJS from 'react-chartjs';
 import axios from 'axios';
+import classNames from 'classnames';
 // const ResponsiveReactGridLayout = ReactGridLayout.Responsive;
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
+const DonutChart = ChartJS.Doughnut;
 
 const LayoutLarge = [
   {i: 'digital', x: 0, y: 0, w: 4,  h: 4, maxW: 4},
@@ -45,20 +48,50 @@ class ChannelDashboard extends React.Component{
     super()
 
     this.state = {
-      data: [
-        {x: "", y: 5},
-        {x: "", y: 0}
-      ]
+      data: this.getData(),
+      digitalData: [{
+        'chart':[]
+      }],
+    digitalDataChart:[{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''}],
+      callCenterData: [{}],
+          callCenterDataChart:[{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''}],
+      inStoreData: [{}],
+          inStoreDataChart:[{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''},{customerSatisfaction: '',customerEffor: ''}]
     }
   }
-
+  getData() {
+      const rand = () => Math.max(Math.floor(Math.random() * 10000), 1000);
+      return [
+        { x: "", y: rand()},
+        { x: "", y: rand()}
+      ];
+    }
   componentDidMount(){
-    this.setState({
-      data: [
-        {x: "", y: 5},
-        {x: "", y: 5}]
-    })
+
+    this.setStateInterval = window.setTimeout(() => {
+      this.setState({
+        data: this.getData()
+      });
+    }, 1000);
+    axios.get('./channelDashboard.json')
+      .then(function(result){
+        this.setState({
+          digitalData: result.data.channelDashboard.satisfactionrate[0],
+          digitalDataChart: result.data.channelDashboard.satisfactionrate[0].chart,
+          callCenterData: result.data.channelDashboard.satisfactionrate[1],
+          callCenterDataChart: result.data.channelDashboard.satisfactionrate[1].chart,
+          inStoreData: result.data.channelDashboard.satisfactionrate[2],
+          inStoreDataChart: result.data.channelDashboard.satisfactionrate[2].chart,
+
+        })
+        console.log(result.data.channelDashboard.satisfactionrate[1].chart)
+      }.bind(this));
+
   }
+
+  componentWillUnmount() {
+  window.clearInterval(this.setStateInterval);
+}
 
   // http://www.codeblocq.com/2015/12/Easy-dynamic-classes-with-React/
   computeClassColor(val){
@@ -93,15 +126,14 @@ class ChannelDashboard extends React.Component{
 
                       <div className="donut-chart-title">Digital</div>
                       <div className="donut-chart">
-                        <div className="donut-rating">5</div>
-                      <VictoryPie animate={{
-                          duration: 1000}}
-                        innerRadius={80}
-                        data={this.state.data}
-                        colorScale={[
-                        "#d5d5d5",
-                        "#F66D3B"]} >
-                      </VictoryPie>
+                        <div ref="donutRating" className={"donut-rating " + this.computeClassColor(this.state.digitalData.overall_score)}>{this.state.digitalData.overall_score}</div>
+                          <VictoryPie animate={{
+                             duration: 1000}}
+                           innerRadius={80}
+                           data={this.state.data}
+                           colorScale={[
+                           "#d5d5d5",
+                           "#F66D3B"]} ></VictoryPie>
                       </div>
                     <table className="table table-bordered">
                       <thead>
@@ -112,39 +144,143 @@ class ChannelDashboard extends React.Component{
                       <tbody>
                         <tr>
                           <th className="col-header" scope="row">Seek</th>
-                          <td className="rating-green">8</td>
-                          <td className="rating-orange">7</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[0].customerSatisfaction)}> {this.state.digitalDataChart[0].customerSatisfaction}</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[0].customerEffor)}>{this.state.digitalDataChart[0].customerEffor}</td>
                         </tr>
                         <tr>
                           <th className="col-header" scope="row">Purchase</th>
-                            <td className="rating-red">3</td>
-                            <td className="rating-green">9</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[1].customerSatisfaction)}> {this.state.digitalDataChart[1].customerSatisfaction}</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[1].customerEffor)}>{this.state.digitalDataChart[1].customerEffor}</td>
                         </tr>
                         <tr>
-                          <th className="col-header" scope="row">Get Help</th><td>Larry</td><td>the Bird</td>
+                          <th className="col-header" scope="row">Get Help</th>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[2].customerSatisfaction)}> {this.state.digitalDataChart[2].customerSatisfaction}</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[2].customerEffor)}>{this.state.digitalDataChart[2].customerEffor}</td>
                         </tr>
                         <tr>
-                          <th className="col-header" scope="row">Optimize</th><td>Larry</td><td>the Bird</td>
+                          <th className="col-header" scope="row">Optimize</th>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[3].customerSatisfaction)}> {this.state.digitalDataChart[3].customerSatisfaction}</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[3].customerEffor)}>{this.state.digitalDataChart[3].customerEffor}</td>
                         </tr>
                         <tr>
-                          <th className="col-header" scope="row">Seek (External)</th><td>Larry</td><td>the Bird</td>
+                          <th className="col-header" scope="row">Seek (External)</th>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[4].customerSatisfaction)}> {this.state.digitalDataChart[4].customerSatisfaction}</td>
+                            <td className={this.computeClassColor(this.state.digitalDataChart[4].customerEffor)}>{this.state.digitalDataChart[4].customerEffor}</td>
                         </tr>
-                        <tr>
+                        <tr className="fb-theme-header">
                           <th colSpan="3">Top Positive Themes</th>
-                          <tr><td colSpan="3">ljdlfask</td></tr>
+                          <tr><td colSpan="3" className="green-text">{this.state.digitalData.topPositiveThemes}</td></tr>
                         </tr>
-                        <tr>
+                        <tr className="fb-theme-header">
                           <th colSpan="3">Top Negative Themes</th>
-                          <tr><td colSpan="3">ljdlfask</td></tr>
+                          <tr><td colSpan="3" className="red-text">{this.state.digitalData.topNegativeThemes}</td></tr>
                         </tr>
                       </tbody>
                     </table>
                   </div>
                   <div className="grid-item" key={'callCenter'}>
-                    2
+
+                          <div className="donut-chart-title">Call Center</div>
+                          <div className="donut-chart">
+                            <div ref="donutRating" className={"donut-rating " + this.computeClassColor(this.state.callCenterData.overall_score)}>{this.state.callCenterData.overall_score}</div>
+                              <VictoryPie animate={{
+                                 duration: 1000}}
+                               innerRadius={80}
+                               data={this.state.data}
+                               colorScale={[
+                               "#d5d5d5",
+                               "#658F2F"]} >
+                             </VictoryPie>
+                          </div>
+                        <table className="table table-bordered">
+                          <thead>
+                            <tr className="fb-table-header">
+                              <th></th><th>Customer Satisfaction</th><th>Ease of Doing Business</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th className="col-header" scope="row">Seek</th>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[0].customerSatisfaction)}> {this.state.callCenterDataChart[0].customerSatisfaction}</td>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[0].customerEffor)}>{this.state.callCenterDataChart[0].customerEffor}</td>
+                            </tr>
+                            <tr>
+                              <th className="col-header" scope="row">Purchase</th>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[1].customerSatisfaction)}> {this.state.callCenterDataChart[1].customerSatisfaction}</td>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[1].customerEffor)}>{this.state.callCenterDataChart[1].customerEffor}</td>
+                            </tr>
+                            <tr>
+                              <th className="col-header" scope="row">Get Help</th>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[2].customerSatisfaction)}> {this.state.callCenterDataChart[2].customerSatisfaction}</td>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[2].customerEffor)}>{this.state.callCenterDataChart[2].customerEffor}</td>
+                            </tr>
+                            <tr>
+                              <th className="col-header" scope="row">Optimize</th>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[3].customerSatisfaction)}> {this.state.callCenterDataChart[3].customerSatisfaction}</td>
+                                <td className={this.computeClassColor(this.state.callCenterDataChart[3].customerEffor)}>{this.state.callCenterDataChart[3].customerEffor}</td>
+                            </tr>
+                            <tr className="fb-theme-header">
+                              <th colSpan="3">Top Positive Themes</th>
+                              <tr><td colSpan="3" className="green-text">{this.state.callCenterData.topPositiveThemes}</td></tr>
+                            </tr>
+                            <tr className="fb-theme-header">
+                              <th colSpan="3">Top Negative Themes</th>
+                              <tr><td colSpan="3" className="red-text">{this.state.callCenterData.topNegativeThemes}</td></tr>
+                            </tr>
+                          </tbody>
+                        </table>
                   </div>
                   <div className="grid-item" key={'inStore'}>
-                    3
+
+                        <div className="donut-chart-title">Call Center</div>
+                        <div className="donut-chart">
+                          <div ref="donutRating" className={"donut-rating " + this.computeClassColor(this.state.inStoreData.overall_score)}>{this.state.inStoreData.overall_score}</div>
+                            <VictoryPie animate={{
+                               duration: 1000}}
+                             innerRadius={80}
+                             data={this.state.data}
+                             colorScale={[
+                             "#d5d5d5",
+                             "#EE9825"]} >
+                           </VictoryPie>
+                        </div>
+                      <table className="table table-bordered">
+                        <thead>
+                          <tr className="fb-table-header">
+                            <th></th><th>Customer Satisfaction</th><th>Ease of Doing Business</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th className="col-header" scope="row">Seek</th>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[0].customerSatisfaction)}> {this.state.inStoreDataChart[0].customerSatisfaction}</td>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[0].customerEffor)}>{this.state.inStoreDataChart[0].customerEffor}</td>
+                          </tr>
+                          <tr>
+                            <th className="col-header" scope="row">Purchase</th>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[1].customerSatisfaction)}> {this.state.inStoreDataChart[1].customerSatisfaction}</td>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[1].customerEffor)}>{this.state.inStoreDataChart[1].customerEffor}</td>
+                          </tr>
+                          <tr>
+                            <th className="col-header" scope="row">Get Help</th>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[2].customerSatisfaction)}> {this.state.inStoreDataChart[2].customerSatisfaction}</td>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[2].customerEffor)}>{this.state.inStoreDataChart[2].customerEffor}</td>
+                          </tr>
+                          <tr>
+                            <th className="col-header" scope="row">Optimize</th>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[3].customerSatisfaction)}> {this.state.inStoreDataChart[3].customerSatisfaction}</td>
+                              <td className={this.computeClassColor(this.state.inStoreDataChart[3].customerEffor)}>{this.state.inStoreDataChart[3].customerEffor}</td>
+                          </tr>
+                          <tr className="fb-theme-header">
+                            <th colSpan="3">Top Positive Themes</th>
+                            <tr><td colSpan="3" className="green-text">{this.state.inStoreData.topPositiveThemes}</td></tr>
+                          </tr>
+                          <tr className="fb-theme-header">
+                            <th colSpan="3">Top Negative Themes</th>
+                            <tr><td colSpan="3" className="red-text">{this.state.inStoreData.topNegativeThemes}</td></tr>
+                          </tr>
+                        </tbody>
+                      </table>
                   </div>
 
                 </ResponsiveReactGridLayout>
