b953d27d10f6173ab66d1a0c28762a2bcc2ecdb5
Knif, Peter (US - New York) <pknif@deloitte.com>
2016-09-09 13:36:53 -0400

    added concat to the sass gulp task

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var flatten = require('gulp-flatten');
+var concat = require('gulp-concat');
 var opts = {
 	includePaths: [
 		'app/components',
@@ -14,6 +15,7 @@ module.exports = () => {
 		.pipe(sourcemaps.init())
 		.pipe(sass(opts).on('error', sass.logError))
 		.pipe(sourcemaps.write('./maps'))
+		.pipe(concat('main.css'))
 		.pipe(flatten())
 		.pipe(gulp.dest('./www/assets/css'));
 };
