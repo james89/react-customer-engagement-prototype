8387c3ec0091e01b49b1064f1da774ef37d08e06
jrothpearl <jrothpearl@deloitte.com>
2016-09-12 11:24:46 -0400

    feedback confidence svg update, borders

--- app/components/feedbackConfidenceRow/feedback-confidence.jsx
+++ app/components/feedbackConfidenceRow/feedback-confidence.jsx
@@ -14,84 +14,80 @@ class FeedbackConfidenceRow extends React.Component {
       <div className="row row-eq-height white-bg">
         <div className="col-md-1 row-header"><span className="sprite sprite-feedback-conf"></span><span className="row-title">Feedback<br />Confidence</span></div>
         <div className="col-md-1">
-          <svg width="39px" height="33px" viewBox="558 0 39 33" version="1.1" >
+          <svg width="28px" height="24px" viewBox="815 0 28 24" version="1.1">
             <defs />
-            <g id="Chart" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(577.500000, 16.500000) rotate(-90.000000) translate(-577.500000, -16.500000) translate(561.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#F47C6D" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
+            <g id="Red" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(815.000000, 0.000000)">
+              <rect id="Rectangle-9" fill="#D0021B" x="0.921151417" y="11.9144295" width="5.30969886" height="11.7785235" />
+              <rect id="Rectangle-9" fill="#D9D9D9" x="11.5405491" y="5.57214765" width="5.30969886" height="17.6677852" />
+              <rect id="Rectangle-9" fill="#D9D9D9" x="22.1599468" y="0.13590604" width="5.30969886" height="23.557047" />
             </g>
           </svg>
         </div>
         <div className="col-md-1">
-          <svg width="39px" height="33px" viewBox="446 0 39 33" version="1.1">
-            <defs />
-            <g id="Chart-Green" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(465.500000, 16.500000) rotate(-90.000000) translate(-465.500000, -16.500000) translate(449.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#CBD959" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-            </g>
+          <svg width="27px" height="24px" viewBox="272 0 27 24" version="1.1">
+              <g id="Green" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(272.000000, 0.000000)">
+                  <rect id="Rectangle-9" fill="#85BC40" x="0.123553931" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="10.7429516" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="21.3623494" y="0.13590604" width="5.30969886" height="23.557047"></rect>
+              </g>
           </svg>
         </div>
         <div className="col-md-1">
-          <svg width="39px" height="33px" viewBox="781 0 39 33" version="1.1" >
+          <svg width="28px" height="24px" viewBox="815 0 28 24" version="1.1">
             <defs />
-            <g id="Chart-" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(800.500000, 16.500000) rotate(-90.000000) translate(-800.500000, -16.500000) translate(784.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
+            <g id="Red" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(815.000000, 0.000000)">
+              <rect id="Rectangle-9" fill="#D0021B" x="0.921151417" y="11.9144295" width="5.30969886" height="11.7785235" />
+              <rect id="Rectangle-9" fill="#D9D9D9" x="11.5405491" y="5.57214765" width="5.30969886" height="17.6677852" />
+              <rect id="Rectangle-9" fill="#D9D9D9" x="22.1599468" y="0.13590604" width="5.30969886" height="23.557047" />
             </g>
           </svg>
         </div>
         <div className="col-md-1" >
-          <svg width="39px" height="33px" viewBox="446 0 39 33" version="1.1">
-            <defs />
-            <g id="Chart-Green" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(465.500000, 16.500000) rotate(-90.000000) translate(-465.500000, -16.500000) translate(449.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#CBD959" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-            </g>
+          <svg width="27px" height="24px" viewBox="272 0 27 24" version="1.1">
+              <g id="Green" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(272.000000, 0.000000)">
+                  <rect id="Rectangle-9" fill="#85BC40" x="0.123553931" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="10.7429516" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="21.3623494" y="0.13590604" width="5.30969886" height="23.557047"></rect>
+              </g>
           </svg>
         </div>
         <div className="col-md-1" >
-          <svg width="39px" height="33px" viewBox="446 0 39 33" version="1.1">
-            <defs />
-            <g id="Chart-Green" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(465.500000, 16.500000) rotate(-90.000000) translate(-465.500000, -16.500000) translate(449.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#CBD959" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#CBD959" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-            </g>
+          <svg width="27px" height="24px" viewBox="0 0 27 24" version="1.1">
+              <g id="Yellow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                  <rect id="Rectangle-9" fill="#F4CF10" x="0.315991834" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                  <rect id="Rectangle-9" fill="#F4CF10" x="10.9353896" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                  <rect id="Rectangle-9" fill="#DADADA" x="21.5547873" y="0.13590604" width="5.30969886" height="23.557047"></rect>
+              </g>
           </svg>
         </div>
         <div className="col-md-1" >
-          <svg width="39px" height="33px" viewBox="558 0 39 33" version="1.1" >
-            <defs />
-            <g id="Chart" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(577.500000, 16.500000) rotate(-90.000000) translate(-577.500000, -16.500000) translate(561.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#F47C6D" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-            </g>
+          <svg width="27px" height="24px" viewBox="272 0 27 24" version="1.1">
+              <g id="Green" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(272.000000, 0.000000)">
+                  <rect id="Rectangle-9" fill="#85BC40" x="0.123553931" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="10.7429516" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                  <rect id="Rectangle-9" fill="#85BC40" x="21.3623494" y="0.13590604" width="5.30969886" height="23.557047"></rect>
+              </g>
           </svg>
         </div>
         <div className="col-md-1" >
-          <svg width="39px" height="33px" viewBox="781 0 39 33" version="1.1" >
-            <defs />
-            <g id="Chart-" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(800.500000, 16.500000) rotate(-90.000000) translate(-800.500000, -16.500000) translate(784.000000, -3.000000)">
-              <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-              <rect id="Rectangle-path" fill="#FBE162" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
+        <svg width="27px" height="24px" viewBox="0 0 27 24" version="1.1">
+            <g id="Yellow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <rect id="Rectangle-9" fill="#F4CF10" x="0.315991834" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                <rect id="Rectangle-9" fill="#F4CF10" x="10.9353896" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                <rect id="Rectangle-9" fill="#DADADA" x="21.5547873" y="0.13590604" width="5.30969886" height="23.557047"></rect>
             </g>
-          </svg>
+        </svg>
 
         </div>
-        <div className="col-md-1" >          <svg width="39px" height="33px" viewBox="558 0 39 33" version="1.1" >
-                    <defs />
-                    <g id="Chart" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd" transform="translate(577.500000, 16.500000) rotate(-90.000000) translate(-577.500000, -16.500000) translate(561.000000, -3.000000)">
-                      <rect id="Rectangle-path" fill="#D0D0CE" x="0.0821444954" y="29.7977463" width="32.3279587" height="9.18508209" />
-                      <rect id="Rectangle-path" fill="#D0D0CE" x="0.149082569" y="14.8432836" width="18.1270986" height="9.18508209" />
-                      <rect id="Rectangle-path" fill="#F47C6D" x="0.149082569" y={0} width="11.5784977" height="9.18508209" />
-                    </g>
-                  </svg></div>
+        <div className="col-md-1" >
+          <svg width="27px" height="24px" viewBox="0 0 27 24" version="1.1">
+              <g id="Yellow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                  <rect id="Rectangle-9" fill="#F4CF10" x="0.315991834" y="11.9144295" width="5.30969886" height="11.7785235"></rect>
+                  <rect id="Rectangle-9" fill="#F4CF10" x="10.9353896" y="5.57214765" width="5.30969886" height="17.6677852"></rect>
+                  <rect id="Rectangle-9" fill="#DADADA" x="21.5547873" y="0.13590604" width="5.30969886" height="23.557047"></rect>
+              </g>
+            </svg>
+          </div>
       </div>
     );
   }
